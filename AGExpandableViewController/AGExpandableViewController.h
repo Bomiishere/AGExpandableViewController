cecdbd10a6dfa038c6d83bcefe0caecf1a8176e8
Bomi <bomiishere@gmail.com>
2019-09-06 18:15:04 +0800

    Initial Commit

---
+++ AGExpandableViewController/AGExpandableViewController.h
@@ -0,0 +1,19 @@
+//
+//  AGExpandableViewController.h
+//  AGExpandableViewController
+//
+//  Created by Bomi on 2019/9/6.
+//  Copyright © 2019 PrototypeC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AGExpandableViewController.
+FOUNDATION_EXPORT double AGExpandableViewControllerVersionNumber;
+
+//! Project version string for AGExpandableViewController.
+FOUNDATION_EXPORT const unsigned char AGExpandableViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AGExpandableViewController/PublicHeader.h>
+
+
